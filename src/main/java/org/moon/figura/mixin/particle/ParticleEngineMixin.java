1c5e7ae22adba054f593d27970caeb09a2733471
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-02-20 20:46:46 -0600

    Improved the ParticleEngineMixin Using @ModifyVariable instead of @Inject with Capture locals, better practice and for mod compatibility in general.

--- src/main/java/org/moon/figura/mixin/particle/ParticleEngineMixin.java
+++ src/main/java/org/moon/figura/mixin/particle/ParticleEngineMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -25,9 +26,10 @@ public abstract class ParticleEngineMixin implements ParticleEngineAccessor {
 
     @Unique private final HashMap<Particle, UUID> particleMap = new HashMap<>();
 
-    @Inject(at = @At(value = "INVOKE", target = "Ljava/util/Iterator;remove()V"), method = "tickParticleList", locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void tickParticleList(Collection<Particle> particles, CallbackInfo ci, Iterator<Particle> iterator, Particle particle) {
+    @ModifyVariable(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleEngine;tickParticle(Lnet/minecraft/client/particle/Particle;)V"), method = "tickParticleList", index = 3)
+    private Particle tickParticleList(Particle particle) {
         particleMap.remove(particle);
+        return particle;
     }
 
     @Override @Intrinsic
