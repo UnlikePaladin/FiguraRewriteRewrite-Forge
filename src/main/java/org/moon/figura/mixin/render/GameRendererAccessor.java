1ad8fbd915b7e4cf849b20f5739fd568165adfed
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-04-04 13:22:27 -0600

    Fixed forge 1.19.3

--- src/main/java/org/moon/figura/mixin/render/GameRendererAccessor.java
+++ src/main/java/org/moon/figura/mixin/render/GameRendererAccessor.java
@@ -12,7 +12,6 @@ public interface GameRendererAccessor {
     @Intrinsic
     @Accessor("EFFECTS")
     static ResourceLocation[] getEffects() {
-        System.out.println("Defaulted");
         return new ResourceLocation[]{new ResourceLocation("shaders/post/notch.json"), new ResourceLocation("shaders/post/fxaa.json"), new ResourceLocation("shaders/post/art.json"), new ResourceLocation("shaders/post/bumpy.json"), new ResourceLocation("shaders/post/blobs2.json"), new ResourceLocation("shaders/post/pencil.json"), new ResourceLocation("shaders/post/color_convolve.json"), new ResourceLocation("shaders/post/deconverge.json"), new ResourceLocation("shaders/post/flip.json"), new ResourceLocation("shaders/post/invert.json"), new ResourceLocation("shaders/post/ntsc.json"), new ResourceLocation("shaders/post/outline.json"), new ResourceLocation("shaders/post/phosphor.json"), new ResourceLocation("shaders/post/scan_pincushion.json"), new ResourceLocation("shaders/post/sobel.json"), new ResourceLocation("shaders/post/bits.json"), new ResourceLocation("shaders/post/desaturate.json"), new ResourceLocation("shaders/post/green.json"), new ResourceLocation("shaders/post/blur.json"), new ResourceLocation("shaders/post/wobble.json"), new ResourceLocation("shaders/post/blobs.json"), new ResourceLocation("shaders/post/antialias.json"), new ResourceLocation("shaders/post/creeper.json"), new ResourceLocation("shaders/post/spider.json")};
     }
 }
